4b332582d462aaa1e77c707e1717c3218ec667db
victoraixala <victoraixala@catalunyaemail.cat>
2023-07-15 16:42:21 +0200

    Syntax fix

--- features/novapersona/src/main/java/com/aldarius/novapersona/FragmentDia.kt
+++ features/novapersona/src/main/java/com/aldarius/novapersona/FragmentDia.kt
@@ -15,7 +15,7 @@ class FragmentDia : DialogFragment()  {
         val year = c.get(Calendar.YEAR)
         val month = c.get(Calendar.MONTH)
         val day = c.get(Calendar.DAY_OF_MONTH)
-        return DatePickerDialog(activity, null, year - 18, month, day)
+        return DatePickerDialog(this.requireContext(), null, year - 18, month, day)
     }
 
 
